88fa7f566d27ff6bcb3c55722c287ca3fc5813fe
santhose1824 <112080726+santhose1824@users.noreply.github.com>
2022-12-24 17:11:12 +0530

    Add files via upload

---
+++ Prevalent.java
@@ -0,0 +1,10 @@
+package com.santhose.shopping.Prevalent;
+
+import com.santhose.shopping.Model.Users;
+
+public class Prevalent {
+    public static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
